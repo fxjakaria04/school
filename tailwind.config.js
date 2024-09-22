d3510398d92cc9298530d33b5c5581208380bfac
fxjakaria04 <freelancerx2023@gmail.com>
2024-09-22 15:20:57 +0600

    config changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ['application.html', 'index.html'],
+  content: ['application.html', 'index.html', 'about.html'],
   theme: {
     extend: {},
   },
