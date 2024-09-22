eadb12667a3274d97189c30723c7aff8798ab299
fxjakaria04 <freelancerx2023@gmail.com>
2024-09-22 16:51:18 +0600

    new page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ['application.html', 'index.html', 'about.html'],
+  content: ['application.html', 'index.html', 'about.html', 'partnership.html'],
   theme: {
     extend: {},
   },
